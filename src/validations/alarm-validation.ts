4cb720a4a9d0e1015d5646166d177612ba49fa0a
Owennn04 <owentengkawan@gmail.com>
2025-12-09 09:57:37 +0700

    initialize

--- src/validations/alarm-validation.ts
+++ src/validations/alarm-validation.ts
@@ -1,14 +1,15 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const createAlarmSchema = z.object({
-  time: z.string().regex(
-    /^([0-1][0-9]|2[0-3]):[0-5][0-9]$/,
-    'Format waktu harus HH:mm (contoh: 07:30)'
-  ),
-  label: z.string().min(1, 'Label alarm harus diisi'),
-  days: z.array(z.boolean()).length(7, 'Days harus array 7 boolean')
-});
+export class AlarmValidation {
+  static readonly CREATE = z.object({
+    time: z.string().min(1),
+    label: z.string().max(100).optional(),
+    dayys: z.array(z.boolean()).length(7),
+    isActive: z.boolean().optional()
+  });
 
-export const toggleAlarmSchema = z.object({
-  isActive: z.boolean()
-});
+  static readonly  UPDATE = z.object({
+    id: z.number().positive(),
+    isActive: z.boolean()
+  });
+}
