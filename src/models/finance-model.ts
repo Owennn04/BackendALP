ddc96db0a3dc59a40b66128617e48b20db652c34
Po <hendrawansaputro06@gmail.com>
2025-12-12 13:44:01 +0700

    tambah finance
    bisa create dan read

---
+++ src/models/finance-model.ts
@@ -0,0 +1,26 @@
+import { Transaction } from "@prisma/client";
+
+export type TransactionResponse = {
+    id: number;
+    type: string;
+    amount: number;
+    category: string;
+    date: Date;
+}
+
+export type CreateTransactionRequest = {
+    type: "INCOME" | "EXPENSE";
+    amount: number;
+    category: string;
+    date: string; // ISO String dari Client
+}
+
+export function toTransactionResponse(transaction: Transaction): TransactionResponse {
+    return {
+        id: transaction.id,
+        type: transaction.type,
+        amount: transaction.amount,
+        category: transaction.category,
+        date: transaction.date
+    }
+}
